c7cf9015cbff397525b5fd4de67ab10e5fc2e225
James Carlson <jxxcarlson@mac.com>
2018-04-14 00:14:59 -0400

    1

--- src/View/ReaderPage.elm
+++ src/View/ReaderPage.elm
@@ -7,11 +7,12 @@ import Element.Events exposing (onClick, onInput)
 import Element.Keyed
 import View.Stylesheet exposing (..)
 import Model exposing (Model)
+import Msg exposing(Msg)
 import View.Widget as Widget
 import View.Menubar as Menubar
 import View.Footer as Footer
 
-
+view : Model -> (Element.Element MyStyles variation Msg)
 view model =
     Element.column Main
         [ width <| px <| toFloat <| model.windowWidth, height <| px <| toFloat <| model.windowHeight ]
@@ -24,7 +25,7 @@ view model =
 
 -- inputField label_ value_ width_ action
 
-
+mainRow : Model -> Element.Element MyStyles variation msg
 mainRow model =
     row Main
         [ height fill ]
@@ -32,12 +33,12 @@ mainRow model =
         , column Main [ width (fillPortion 70), height fill ] [ text "READER IN PAGE" ]
         ]
 
-
+menuContent : List (Element.Element MyStyles variation msg)
 menuContent =
     [ el Menubar [ verticalCenter, paddingLeft 20, paddingRight 20 ] (text "Sign in Menubar")
     ]
 
-
+footerContent : List (Element.Element MyStyles variation msg)
 footerContent =
     [ el Menubar [ verticalCenter, paddingLeft 20, paddingRight 20 ] (text "Sign in Footer")
     ]
